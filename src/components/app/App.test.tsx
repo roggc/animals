1bb0d4e2d1f28e4734c39173f0f30838c1045afc
roggc <roggc9@gmail.com>
2021-08-12 22:05:50 +0200

    animals lovers blog

--- src/components/app/App.test.tsx
+++ src/components/app/App.test.tsx
@@ -4,12 +4,7 @@ import { render } from '@testing-library/react'
 import {App} from './'
 
 describe('App', () => {
-    // test('true is truthy', () => {
-    //   expect(true).toBe(true);
-    // });
-   
     it('renders', () => {
-      //expect(false).toBe(false);
       render(<App />)
     });
   });
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,86 @@
-import React from 'react'
+import React,{useMemo} from 'react'
+import styled from 'styled-components'
+import {users} from '../../assets'
 
-interface IAppProps{
-
+interface IUser {
+    id: string;
+    name: {
+        given: string;
+        surname: string;
+    };
+    points: number;
+    animals: string[];
+    isActive: boolean;
+    age: number;
 }
+
+interface IAppProps{}
 
 export const App:React.FC<IAppProps>=({})=>{
+
+    /**
+     * this calculates the list of different available animals in users
+     */
+    const availableAnimals=useMemo(()=>{
+        const animals:string[]=[]
+        users.forEach((user)=>{
+            user.animals.forEach((animal)=>{
+                if(!animals.find((animal_)=>animal===animal_)){
+                    animals.push(animal)
+                }
+            })
+        })
+        return animals
+    },[users])
+
+    const getUsersForAnimalUpToTen=(animal:string)=>{
+        const usersForAnimal:IUser[]=[]
+        users.forEach((user)=>{
+            if(user.isActive){
+                if(user.animals.find(animal_=>animal===animal_)){
+                    usersForAnimal.push(user)
+                }
+            }
+        })
+        return usersForAnimal.sort((a,b)=>b.points-a.points).slice(0,10)
+    }
+
+    const getComponent=(animal:string)=>{
+        return (
+            <ComponentContainer key={animal}>
+                <h2>{animal}</h2>
+                {getUsersForAnimalUpToTen(animal).map(user=>getSubComponent(user))}
+            </ComponentContainer>
+        )
+    }
+
+    const getSubComponent=(user:IUser)=>{
+        return (
+            <SubComponentContainer key={user.id}>
+                <div>{user.name.given}{' '}{user.name.surname}</div>
+                <div>{user.points}{' pts'}</div>
+            </SubComponentContainer>
+        )
+    }
+
     return (
-        <div>hello world! tsx</div>
+        <AppContainer>
+            <h1>Animals lovers blog</h1>
+            {availableAnimals.map(animal=>getComponent(animal))}
+        </AppContainer>
     )
 }
+
+const AppContainer=styled.div`
+font-family:sans-serif;
+`
+
+const SubComponentContainer=styled.div`
+display:flex;
+justify-content:space-between;
+`
+
+const ComponentContainer=styled.div`
+width:fit-content;
+min-width:300px;
+`
